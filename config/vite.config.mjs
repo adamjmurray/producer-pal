45a58cdba4ebe319c6b4c7d72d3ceca51d80446a
Adam Murray <adamjmurray@gmail.com>
2025-10-17 19:32:15 -0700

    get things working with latest version of tailwind

--- config/vite.config.mjs
+++ config/vite.config.mjs
@@ -1,15 +1,10 @@
 // config/vite.config.mjs
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), tailwindcss()],
   root: "webui",
   server: { port: 3355 },
-  css: {
-    postcss: {
-      plugins: [tailwindcss("./config/tailwind.config.mjs")],
-    },
-  },
 });
