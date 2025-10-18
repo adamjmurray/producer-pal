c94713562e75056e0f9bce579aa0daab9d2085a6
Adam Murray <adamjmurray@gmail.com>
2025-10-17 20:44:28 -0700

    build the webui as a single file

--- config/vite.config.mjs
+++ config/vite.config.mjs
@@ -1,9 +1,14 @@
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [preact(), tailwindcss(), viteSingleFile()],
   root: "webui",
   server: { port: 3355 },
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+  },
 });
