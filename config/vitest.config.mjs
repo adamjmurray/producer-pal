88aa29786771a618e3ef13b09dfe09c0c147118c
Adam Murray <adamjmurray@gmail.com>
2025-12-31 09:55:39 -0800

    ratchet up test thresholds

--- config/vitest.config.mjs
+++ config/vitest.config.mjs
@@ -77,10 +77,10 @@ export default defineConfig({
 
       // IMPORTANT: Do NOT let test coverage drop:
       thresholds: {
-        statements: 95.8, // Keep above 95
-        branches: 91.6, // Keep above 91
-        functions: 96.8, // Keep above 96
-        lines: 96.1, // Keep above 96
+        statements: 96, // Keep above 96
+        branches: 91.9, // Keep above 92
+        functions: 96.9, // Keep above 97
+        lines: 96.3, // Keep above 96
       },
     },
   },
