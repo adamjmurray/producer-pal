656997922cbc73cdf57362d495cacb973deb940a
Adam Murray <adamjmurray@gmail.com>
2026-01-19 12:36:57 -0800

    ratchet thresholds

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -92,7 +92,7 @@ export default defineConfig({
       thresholds: {
         statements: 98.3, // Keep above 98
         branches: 94.1, // Keep above 94
-        functions: 98.85, // Keep above 98
+        functions: 99, // Keep above 99
         lines: 98.65, // Keep above 98
       },
     },
