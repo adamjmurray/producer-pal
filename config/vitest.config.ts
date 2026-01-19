abfa04c965fc10488c2d06e1f3c1b307da438244
Adam Murray <adamjmurray@gmail.com>
2026-01-18 23:03:24 -0800

    ratchet coverage

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -88,10 +88,10 @@ export default defineConfig({
 
       // IMPORTANT: Do NOT let test coverage drop:
       thresholds: {
-        statements: 98, // Keep above 98
-        branches: 93.7, // Keep above 93
-        functions: 98.7, // Keep above 98
-        lines: 98.3, // Keep above 98
+        statements: 98.25, // Keep above 98
+        branches: 94, // Keep above 94
+        functions: 98.75, // Keep above 98
+        lines: 98.5, // Keep above 98
       },
     },
   },
